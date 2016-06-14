95dc8895b9796b2a869e59af4877255f40c34cbe
Sniku <sniku@ted.ted.pasazer.info>
2016-06-14 12:03:54 +0200

    initial attempt at rewriting mediawiki_client
    pywiki uses the official mediawiki API instead of web interface

---
+++ pywiki/wiki_client.py
@@ -0,0 +1,319 @@
+import os
+import os.path
+import sys
+import cmd
+import configparser
+import tempfile
+from subprocess import call
+from urllib.parse import urljoin
+
+import furl
+import html2text as html2text
+import requests
+
+
+CONFIG_FILE = os.path.expanduser('~/.config/wiki_client.conf')
+
+
+class MediaWikiEditor(object):
+
+    def open_article(self, initial_content, title=""):
+        if title:  # sanitize for security
+            title = "".join(x for x in title if x.isalnum())
+        prefix = (title or "tmp") + "__["
+
+        with tempfile.NamedTemporaryFile(prefix=prefix, suffix="].tmp.wiki", delete=False,) as tmpfile:
+            tmpfile.write(initial_content.encode('utf8'))
+            tmpfile.flush()
+            editor_with_args = settings['editor'].split(" ") + [tmpfile.name]
+            # print editor_with_args
+            call(editor_with_args)
+            tmpfile.flush()
+            tmpfile.close()
+
+            edited_file = open(tmpfile.name)
+            edited_content = edited_file.read()
+            edited_file.close()
+
+            os.unlink(tmpfile.name)
+
+            return initial_content, edited_content
+
+
+
+class Settings(dict):
+    def __init__(self):
+        super(Settings, self).__init__()
+        if self.check_config_file():
+            self.read_config()
+            self.validate_settings()
+
+    def read_config(self):
+        booleans = ['verbose']
+        config = configparser.ConfigParser()
+        config.read(CONFIG_FILE)
+        options = config.options('defaults')
+        for option in options:
+            if option in booleans:
+                self[option] = config.getboolean('defaults', option)
+            else:
+                self[option] = config.get('defaults', option)
+
+        self['editor'] = self.get('force_editor', None) or os.environ.get('EDITOR', 'vim')
+
+        if self.get('mediawiki_url') and not self.get('mediawiki_api_url'):
+            self['mediawiki_api_url'] = urljoin(self['mediawiki_url'], 'api.php')
+
+    def check_config_file(self):
+        if not os.path.isfile(CONFIG_FILE):
+            raise Exception(u'Config file %s is missing.'%CONFIG_FILE)
+        return True
+
+    def validate_settings(self):
+        if not self.get('mediawiki_url', None):
+            raise Exception("Config directive 'mediawiki_url' is empty of missing. "
+                            "You must provide URL to your mediawiki installation.")
+
+settings = Settings()
+
+
+class ApiClient(object):
+    BASE_URL = settings['mediawiki_api_url']
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.editor = MediaWikiEditor()
+        self.api_login()
+
+    def get_url(self, **kwargs):
+        """
+        helper function for getting a specific API url
+        """
+        url = furl.furl(self.BASE_URL).add({'format': 'json'}).add(kwargs)
+        return url
+
+    def _get_context(self, **kwargs):
+        params = {'format': 'json'}
+        params.update(**kwargs)
+        return params
+
+    def get_base_request(self, **kwargs) -> requests.Request:
+        """
+        creates a Request object with all the required headers set.
+        Use this function to create new Requests.
+        """
+        req = requests.Request(**kwargs)
+
+        if not req.url:
+            req.url = self.BASE_URL
+
+        # Handle HTTP basic authentication
+        if settings.get('http_auth_username', None) and settings.get('http_auth_password', None):
+            username = settings['http_auth_username']
+            password = settings['http_auth_password']
+            req.auth = username, password
+
+        return req
+
+    def execute_request(self, request: requests.Request) -> requests.Response:
+        """
+        Use this function to execute all Api requests
+        """
+        prepared = self.session.prepare_request(request)
+        resp = self.session.send(prepared)
+        return resp
+
+    def do_request(self, **request_kwargs) -> requests.Response:
+        """
+        Convenience function for creating and executing request in one go.
+        """
+        req = self.get_base_request(**request_kwargs)
+        resp = self.execute_request(req)
+        return resp
+
+    def api_login(self):
+        """
+        Performs the authentication to the mediawiki installation.
+        Authentication is somewhat convoluted. Steps
+        1. request a token
+        2. authenticate using username, password and token
+        """
+
+        # Handle Mediawiki authentication
+        if settings.get('mediawiki_username', None) and settings.get('mediawiki_password', None):
+            username = settings['mediawiki_username']
+            password = settings['mediawiki_password']
+
+            # get token
+            context = self._get_context(lgname=username)
+            login_url = self.get_url(action='login', format='json')
+            response = self.do_request(method='POST', data=context, url=login_url)
+
+            # perform the real log in with the token
+            if response.status_code == 200:
+                login_details = response.json()['login']
+
+                context = self._get_context(lgname=username, lgpassword=password, lgtoken=login_details['token'])
+                response = self.do_request(method='POST', data=context, url=login_url)
+                resp_data = response.json()['login']
+                if resp_data['result'] != 'Success':
+                    raise Exception("Unable to log in", response.json())
+            else:
+                raise('Unable to log in', response)
+
+    def save_article(self, title, content, token):
+        """
+        Creates new revision for a given article.
+        """
+        url = self.get_url(action='edit', title=title)
+        context = self._get_context(url=url, text=content, token=token, summary='PyWiki edit')
+        resp = self.do_request(method='POST', data=context)
+        # TODO validate
+
+    def upload_file(self, filepath, token, alt_filename=None):
+        filename = alt_filename or os.path.split(filepath)[1]
+        url = self.get_url(action='upload')
+        file_content = open(filepath, 'rb').read()
+        resp = self.do_request(method='POST', token=token, filename=filename, file=file_content)
+        # TODO validate
+
+    def search(self, phrase):
+        search_url = self.get_url(action='query', list='search', srsearch=phrase)
+        resp = self.do_request(method='GET', url=search_url)
+        if resp.status_code == 200:
+            return resp.json()['query']['search']
+        raise Exception('Search failed', resp)
+
+    def get_page_content(self, title):
+        article_url = self.get_url(action='query', prop='revisions', titles=title, rvprop='content', rvlimit=1)
+
+        resp = self.do_request(method='GET', url=article_url)
+        return list(resp.json()['query']['pages'].values())[0]['revisions'][0]['*']
+
+    def display_article(self, title) -> tuple(str, str):
+        """
+            Display article for given title in an EDITOR.
+            Returns a tuple of (initial_content, new_content)
+        """
+        page = self.get_page_content(title)
+        old_content, new_content = self.editor.open_article(page, title)
+
+        return old_content, new_content
+
+
+class PyWikiCommands(cmd.Cmd):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.prompt = '\nWiki command: '
+        self.api = ApiClient()
+        self.last_search_query = None
+        self.last_search_results = None
+
+    def do_search(self, keyword, quiet=False):
+        """ Search for a keyword
+        """
+        self.last_search_query = keyword
+        self.last_search_results = self.api.search(self.last_search_query)
+        if not quiet:
+            print('Searching for', self.last_search_query)
+        self.display_search_list()
+
+    def display_search_list(self):
+        """ Display the last search result """
+        if not self.last_search_results:
+            print('No results for "%s"' % self.last_search_query)
+        else:
+            # directly display the page
+            if len(self.last_search_results) == 1 and self.last_search_results[0]['what'] == 'perfect_match':
+                print('Opening', self.last_search_query)
+                self.do_go(self.last_search_results[0]['page_name'])
+            else:
+                for index, result in enumerate(self.last_search_results, start=1):
+                    print(index, result['title'], html2text.html2text(result['snippet']))
+                    # print(result['index'], result['title'], '\n\t', result['match'])
+
+    def do_display_search_result(self, index):
+        """ displays the article specified by the index in the search list  """
+        try:
+            index = int(index) - 1 # we enumerated starting on 1
+            hit = self.last_search_results[index]
+            title = hit['title']
+        except IndexError:
+            print('Wrong index - try again')
+        else:
+            print('Opening', title)
+            old_content, new_content = self.api.display_article(hit['title'])
+            if old_content != new_content:
+                self.api.save_article(title, new_content)
+
+
+
+    def do_EOF(self, line):
+        """ quit """
+        print()
+        '\nbye'
+        return True
+
+    def precmd(self, line):
+        if not line or len(line) == 0:
+            return line
+
+        cmd = line.split()[0]
+
+        if line.startswith('/'):
+            line = 'search ' + line[1:]
+        elif cmd.isnumeric():
+            line = 'display_search_result ' + cmd
+
+        return line
+
+    def postloop(self):
+        print()
+
+def run(args):
+    m = PyWikiCommands()
+
+    stdin_data = None
+    interactive = False
+
+    if not sys.stdin.isatty():
+        stdin_data = sys.stdin.read()
+
+        tty = open('/dev/tty', 'r')
+        os.dup2(tty.fileno(), 0)
+
+    if args['<article_name>']:
+
+        if args['append']:
+            if args['<text>']:
+                # append text to extisting article and save
+                text_to_append = args['<text>'].decode('utf8')
+                m.append_to_article_and_save(args['<article_name>'], text_to_append)
+        elif args['log']:
+            if args['<text>']:
+                text_to_log = args['<text>'].decode('utf8')
+                m.log_and_save(args['<article_name>'], text_to_log)
+        elif args["mv"]:
+            m.mv(args['<article_name>'], args['<new_name>'], args['--leave_redirect'])
+        elif stdin_data is not None:
+            stdin_data = stdin_data.decode('utf8')
+            m.append_to_article_and_open(args['<article_name>'], stdin_data)
+        elif args['cat']:
+            m.cat(args['<article_name>'])
+        elif args['<article_name>'][0] == "/":  # search
+            m.do_search(args['<article_name>'][1:])
+            interactive = True
+        else:
+            # just open article
+            m.do_go(args['<article_name>'])
+            interactive = True
+    elif args['upload']:
+            m.do_upload_file(args['<filepath>'], args['<alt_filename>'])
+    else:
+        interactive = True
+
+
+    if interactive:
+        # and go to interactive mode
+        a = m.cmdloop()
